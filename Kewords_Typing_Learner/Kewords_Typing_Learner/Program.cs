31042bd57d297fe7cb3ab4b9427358328b16aa7a
Andrii <andriy.lavreniuk07@gmail>
2023-12-26 22:11:29 +0200

    Created

---
+++ Kewords_Typing_Learner/Kewords_Typing_Learner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kewords_Typing_Learner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
